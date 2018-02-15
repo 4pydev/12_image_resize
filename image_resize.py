f1ba2ded348025ad1ead0dc0ee2e64f4f4d9ed9d
4pydev <4pydev@gmail.com>
2018-02-15 16:53:29 +0300

    Fixed pep8 violations

--- image_resize.py
+++ image_resize.py
@@ -8,18 +8,18 @@ from PIL import Image
 def create_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument('path',
-                        help = "Path to initial image file")
+                        help="Path to initial image file")
     parser.add_argument('-w', '--width',
                         type=int,
-                        help = "Width of resized image")
+                        help="Width of resized image")
     parser.add_argument('-ht', '--height',
                         type=int,
-                        help = "Height of resized image")
+                        help="Height of resized image")
     parser.add_argument('-s', '--scale',
                         type=float,
-                        help = "Scale of resizing image")
+                        help="Scale of resizing image")
     parser.add_argument('-p', '--new_path',
-                        help = "The path you want to save resized image")
+                        help="The path you want to save resized image")
     return parser
 
 
@@ -30,45 +30,47 @@ def load_img(path_to_original_img):
 def get_new_img_size(initial_img, params):
     initial_width, initial_height = initial_img.size
     new_width, new_height, scale = params
-    if scale != None and (new_width == None and new_height == None):
+    if scale is not None and (new_width is None and new_height is None):
         return round(scale * initial_width), round(scale * initial_height)
-    elif scale != None and (new_width != None or new_height != None):
+    elif scale is not None and (new_width is not None or
+                                new_height is not None):
         raise Exception("Wrong parameters! "
                         "There aren't a width (or/and a height) and"
                         " a scale at the same time.")
-    elif scale == None:
-        if new_height == None:
+    elif scale is None:
+        if new_height is None:
             return new_width, round(
                 new_width / initial_width * initial_height)
-        elif new_width == None:
-            return round(new_height / initial_height * 
+        elif new_width is None:
+            return round(new_height / initial_height *
                          initial_width), new_height
         else:
             return new_width, new_height
 
 
 def get_new_img_full_path(path_to_original_img,
-                              new_size, 
-                              path_to_new_img=None):
-    if path_to_new_img == None:
+                          new_size,
+                          path_to_new_img=None):
+    if path_to_new_img is None:
         path_to_new_img = os.path.dirname(path_to_original_img)
     width, height = new_size
     size_string = "_{}x{}.".format(width, height)
     new_img_basename = size_string.join(
             os.path.basename(path_to_original_img).split('.'))
     return "{}/{}".format(path_to_new_img, new_img_basename)
-    
+
 
 def create_new_image_file(initial_img,
-                         new_size, 
-                         new_img_full_path):
+                          new_size,
+                          new_img_full_path):
     initial_img.resize(new_size).save(new_img_full_path)
 
 
 def get_proportions_warning(initial_img, new_size):
     initial_width, initial_height = initial_img.size
     new_width, new_height = new_size
-    if round(initial_width/initial_height, 1) != round(new_width/new_height, 1):
+    if round(initial_width/initial_height, 1) != round(
+                    new_width/new_height, 1):
         print("Warning: Origin image's proportions will change!")
 
 
@@ -80,14 +82,14 @@ if __name__ == '__main__':
     params = (namespace.width, namespace.height, namespace.scale)
     path_to_original_img = namespace.path
     path_to_new_img = namespace.new_path
-    
+
     initial_img = load_img(path_to_original_img)
 
     new_img_size = get_new_img_size(initial_img, params)
-    
+
     new_img_full_path = get_new_img_full_path(path_to_original_img,
-                                                      new_img_size, 
-                                                      path_to_new_img)
+                                              new_img_size,
+                                              path_to_new_img)
 
     get_proportions_warning(initial_img, new_img_size)
     create_new_image_file(initial_img, new_img_size, new_img_full_path)
