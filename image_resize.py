8d152d0ee2c1d86a3be11ca87605af5f793cc6da
4pydev <4pydev@gmail.com>
2018-02-15 15:01:45 +0300

    Realized arguments parsing

--- image_resize.py
+++ image_resize.py
@@ -1,7 +1,26 @@
 #!/usr/bin/env python3
 
 import os.path
+import argparse
 from PIL import Image
+
+
+def create_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path',
+                        help = "Path to initial image file")
+    parser.add_argument('-w', '--width',
+                        type=int,
+                        help = "Width of resized image")
+    parser.add_argument('-ht', '--height',
+                        type=int,
+                        help = "Height of resized image")
+    parser.add_argument('-s', '--scale',
+                        type=float,
+                        help = "Scale of resizing image")
+    parser.add_argument('-p', '--new_path',
+                        help = "The path you want to save resized image")
+    return parser
 
 
 def load_img(path_to_original_img):
@@ -14,7 +33,9 @@ def get_new_img_size(initial_img, params):
     if scale != None and (new_width == None and new_height == None):
         return round(scale * initial_width), round(scale * initial_height)
     elif scale != None and (new_width != None or new_height != None):
-        raise Exception("Wrong parameters!")
+        raise Exception("Wrong parameters! "
+                        "There aren't a width (or/and a height) and"
+                        " a scale at the same time.")
     elif scale == None:
         if new_height == None:
             return round(new_width), round(
@@ -24,25 +45,24 @@ def get_new_img_size(initial_img, params):
                          initial_width), round(new_height)
         else:
             return round(new_width), round(new_height)
-        
-        
-def get_resized_img_full_path(path_to_original_img, 
+
+
+def get_new_img_full_path(path_to_original_img,
                               new_size, 
-                              path_to_resized_img=None):
-    if path_to_resized_img == None or path_to_resized_img == "":
-        path_to_resized_img = os.path.dirname(path_to_original_img)
+                              path_to_new_img=None):
+    if path_to_new_img == None or path_to_new_img == "":
+        path_to_new_img = os.path.dirname(path_to_original_img)
     width, height = new_size
     size_string = "_{}x{}.".format(width, height)
-    resized_img_basename = size_string.join(
+    new_img_basename = size_string.join(
             os.path.basename(path_to_original_img).split('.'))
-    print(resized_img_basename)
-    return "{}/{}".format(path_to_resized_img, resized_img_basename)
+    return "{}/{}".format(path_to_new_img, new_img_basename)
     
 
-def create_resized_image(initial_img, 
+def create_new_image_file(initial_img,
                          new_size, 
-                         resized_img_full_path):
-    initial_img.resize(new_size).save(resized_img_full_path)
+                         new_img_full_path):
+    initial_img.resize(new_size).save(new_img_full_path)
 
 
 def get_proportions_warning(initial_img, new_size):
@@ -53,21 +73,21 @@ def get_proportions_warning(initial_img, new_size):
 
 
 if __name__ == '__main__':
-    
-    # Initial data
-    params = (300, 500, None)
-    path_to_original_img = './img/small-kitten.jpg'
-    path_to_resized_img = "."
-    print(params)
+
+    parser = create_parser()
+    namespace = parser.parse_args()
+
+    params = (namespace.width, namespace.height, namespace.scale)
+    path_to_original_img = namespace.path
+    path_to_new_img = namespace.new_path
     
     initial_img = load_img(path_to_original_img)
 
-    new_img_size = get_new_img_size(initial_img, params)   
-    print("\n")
+    new_img_size = get_new_img_size(initial_img, params)
     
-    resized_img_full_path = get_resized_img_full_path(path_to_original_img, 
+    new_img_full_path = get_new_img_full_path(path_to_original_img,
                                                       new_img_size, 
-                                                      path_to_resized_img)
-    print(resized_img_full_path)
+                                                      path_to_new_img)
+
     get_proportions_warning(initial_img, new_img_size)
-    create_resized_image(initial_img, new_img_size, resized_img_full_path)
+    create_new_image_file(initial_img, new_img_size, new_img_full_path)
