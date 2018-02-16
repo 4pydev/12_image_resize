645a17d406d5f3acc4213b771bdbe132d71655e0
4pydev <4pydev@gmail.com>
2018-02-16 18:40:46 +0300

    fixed remarks 15/02/18

--- image_resize.py
+++ image_resize.py
@@ -5,19 +5,20 @@ from PIL import Image
 
 def create_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('path',
+    parser.add_argument("path",
                         help="Path to initial image file")
-    parser.add_argument('-w', '--width',
+    parser.add_argument("-w", "--width",
                         type=int,
                         help="Width of resized image")
-    parser.add_argument('-ht', '--height',
+    parser.add_argument("-ht", "--height",
                         type=int,
                         help="Height of resized image")
-    parser.add_argument('-s', '--scale',
+    parser.add_argument("-s", "--scale",
                         type=float,
                         help="Scale of resizing image")
-    parser.add_argument('-p', '--new_path',
-                        help="The path you want to save resized image")
+    parser.add_argument("-p", "--new_path",
+                        help="The path to directory you want"
+                             " to save resized image")
     return parser
 
 
@@ -25,21 +26,21 @@ def load_img(path_to_original_img):
     return Image.open(path_to_original_img)
 
 
-def get_new_img_size(initial_img, params):
-    initial_width, initial_height = initial_img.size
+def is_not_correct_params(params):
     new_width, new_height, scale = params
-    if scale is not None and (new_width is None and new_height is None):
+    return scale and (new_width or new_height)
+
+
+def get_new_img_size(initial_img_size, params):
+    initial_width, initial_height = initial_img_size
+    new_width, new_height, scale = params
+    if scale:
         return round(scale * initial_width), round(scale * initial_height)
-    elif scale is not None and (new_width is not None or
-                                new_height is not None):
-        raise Exception("Wrong parameters! "
-                        "There aren't a width (or/and a height) and"
-                        " a scale at the same time.")
-    elif scale is None:
-        if new_height is None:
+    elif not scale:
+        if not new_height:
             return new_width, round(
                 new_width / initial_width * initial_height)
-        elif new_width is None:
+        elif not new_width:
             return round(new_height / initial_height *
                          initial_width), new_height
         else:
@@ -49,12 +50,14 @@ def get_new_img_size(initial_img, params):
 def get_new_img_full_path(path_to_original_img,
                           new_size,
                           path_to_new_img=None):
-    if path_to_new_img is None:
-        path_to_new_img = os.path.dirname(path_to_original_img)
+    if not path_to_new_img:
+        path_to_new_img = os.path.dirname(
+            os.path.abspath(path_to_original_img))
     width, height = new_size
-    size_string = "_{}x{}.".format(width, height)
+    size_string = "_{}x{}".format(width, height)
     new_img_basename = size_string.join(
-            os.path.basename(path_to_original_img).split('.'))
+        os.path.splitext(os.path.basename(path_to_original_img)))
+
     return "{}/{}".format(path_to_new_img, new_img_basename)
 
 
@@ -75,19 +78,33 @@ def get_proportions_warning(initial_img, new_size):
 if __name__ == '__main__':
 
     parser = create_parser()
-    namespace = parser.parse_args()
+    args = parser.parse_args()
 
-    params = (namespace.width, namespace.height, namespace.scale)
-    path_to_original_img = namespace.path
-    path_to_new_img = namespace.new_path
+    params = (
+        args.width,
+        args.height,
+        args.scale
+    )
+    path_to_original_img = args.path
+    path_to_new_img = args.new_path
 
     initial_img = load_img(path_to_original_img)
+    initial_img_size = initial_img.size
 
-    new_img_size = get_new_img_size(initial_img, params)
+    if is_not_correct_params(params):
+        parser.error("Wrong parameters! "
+                     "There aren't a width (or/and a height) and"
+                     " a scale at the same time.")
+    new_img_size = get_new_img_size(initial_img_size, params)
 
-    new_img_full_path = get_new_img_full_path(path_to_original_img,
-                                              new_img_size,
-                                              path_to_new_img)
+    new_img_full_path = get_new_img_full_path(
+        path_to_original_img,
+        new_img_size,
+        path_to_new_img
+    )
 
-    get_proportions_warning(initial_img, new_img_size)
-    create_new_image_file(initial_img, new_img_size, new_img_full_path)
+    get_proportions_warning(initial_img,
+                            new_img_size)
+    create_new_image_file(initial_img,
+                          new_img_size,
+                          new_img_full_path)
